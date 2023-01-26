43c86652fd6a20cbc0a8a138533adafe85a7d67f
brandy._ <tmdal0303@naver.com>
2023-01-26 11:58:02 +0900

    230126

--- src/main/kotlin/com/example/storyapi_sm2/dao/FileDao.kt
+++ src/main/kotlin/com/example/storyapi_sm2/dao/FileDao.kt
@@ -4,7 +4,7 @@ import com.example.storyapi_sm2.domain.entity.File
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository
+
 interface FileDao: JpaRepository<File, String> {
 
 }
